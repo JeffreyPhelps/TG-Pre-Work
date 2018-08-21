279f6468a6b6a7e58fe1ab551d373b80913e8651
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-21 09:08:48 -0600

    Codecademy - Learn Javascript, Introduction to JavaScript, Exercise 7, Libraries

--- script.js
+++ script.js
@@ -29,8 +29,15 @@
 
 
 // Codecademy - Learn Javascript, Introduction to JavaScript, Exercise 6, Built-in Methods
-console.log('Codecademy'.toUpperCase());
-console.log('    Remove whitespace   '.trim());
+// console.log('Codecademy'.toUpperCase());
+// console.log('    Remove whitespace   '.trim());
+
+
+// Codecademy - Learn Javascript, Introduction to JavaScript, Exercise 7, Libraries
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
 
 
 
