b94f3dd6a4ea1b8948e3b64f249f6520a3392f57
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-25 14:04:59 -0600

    Codecademy - Learn Javascript, Functions, Exercise 6, Return II

--- script.js
+++ script.js
@@ -282,22 +282,50 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Functions, Exercise 5, Return
-let orderCount = 0;
-const takeOrder = (topping, crustType) => {
-  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-  orderCount++;
-};
+// let orderCount = 0;
+// const takeOrder = (topping, crustType) => {
+//   console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+//   orderCount++;
+// };
 
-const getSubTotal = (itemCount) => {
-	return itemCount * 7.5;
-}
+// const getSubTotal = (itemCount) => {
+// 	return itemCount * 7.5;
+// }
+
+// takeOrder('mushroom', 'thin crust');
+// takeOrder('spinach', 'whole wheat');
+// takeOrder('pepperoni', 'brooklyn style');
+
+// console.log(getSubTotal(orderCount));
+
+
+// Codecademy - Learn Javascript, Functions, Exercise 6, Return II
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount++;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
 
 takeOrder('mushroom', 'thin crust');
 takeOrder('spinach', 'whole wheat');
 takeOrder('pepperoni', 'brooklyn style');
 
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+const getTax = (itemCount) => {
+  return getSubTotal(itemCount) * 0.06
+}
+
+const getTotal = (itemCount) => {
+  return getTax(itemCount) + getSubTotal(itemCount)
+}
+
 console.log(getSubTotal(orderCount));
 
+console.log(getTotal(orderCount))
 
 
 
