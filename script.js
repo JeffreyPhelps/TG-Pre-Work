af6b30c235ace29f2653cb59ba3fc538e7bdc657
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-26 10:35:34 -0600

    Codecademy - Learn Javascript, Scope, Exercise 3, Global Scope II

--- script.js
+++ script.js
@@ -372,15 +372,30 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Scope, Exercise 2, Global Scope
+// const satellite = 'The Moon';
+// const galaxy = 'The Milky Way';
+// let stars = 'North Star';
+
+// const myNightSky = () => {
+//   return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+// }
+
+// console.log(myNightSky());
+
+
+// Codecademy - Learn Javascript, Scope, Exercise 3, Global Scope II
 const satellite = 'The Moon';
 const galaxy = 'The Milky Way';
 let stars = 'North Star';
 
 const myNightSky = () => {
+  stars = 'Sirius';
   return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 }
 
 console.log(myNightSky());
+console.log(stars);
+
 
 
 
