3b8aa14371e9930d4a58bc114e8dd097b7571a4e
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 14:42:11 -0600

    Codecademy - Learn Javascript, Iterators, Exercise 7, Review: Iterators

--- script.js
+++ script.js
@@ -658,19 +658,20 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Iterators, Exercise 6, Choose the Right Iterator
-let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+// let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+// let nums = [1, 50, 75, 200, 350, 525, 1000];
+// //  Choose a method that will return undefined
+// cities.forEach(city => console.log('Have you visited ' + city + '?'));
+// // Choose a method that will return a new array
+// let longCities = cities.filter(city => city.length > 7);
+// // Choose a method that will return a new array
+// let smallerNums = nums.map(num => num - 5);
+// // Choose a method that will return a boolean value
+// nums.every(num => num < 0);
 
-let nums = [1, 50, 75, 200, 350, 525, 1000];
 
-//  Choose a method that will return undefined
-cities.forEach(city => console.log('Have you visited ' + city + '?'));
+// Codecademy - Learn Javascript, Iterators, Exercise 7, Review: Iterators
+// N/A
 
-// Choose a method that will return a new array
-let longCities = cities.filter(city => city.length > 7);
 
-// Choose a method that will return a new array
-let smallerNums = nums.map(num => num - 5);
-
-// Choose a method that will return a boolean value
-nums.every(num => num < 0);
 
