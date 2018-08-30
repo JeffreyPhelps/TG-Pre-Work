499c6b1a3b6358840a853c81ad78d47b8b9eafc3
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 21:04:38 -0600

    Codecademy - Learn Javascript, Classes, Exercise 10, Inheritance V

--- script.js
+++ script.js
@@ -1152,6 +1152,39 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Classes, Exercise 9, Inheritance IV
+// class HospitalEmployee {
+//     constructor(name) {
+//     this._name = name;
+//     this._remainingVacationDays = 20;
+//     }
+    
+//     get name() {
+//     return this._name;
+//     }
+    
+//     get remainingVacationDays() {
+//     return this._remainingVacationDays;
+//     }
+    
+//     takeVacationDays(daysOff) {
+//     this._remainingVacationDays -= daysOff;
+//     }
+// }
+
+// class Nurse extends HospitalEmployee {
+// constructor(name, certifications) {
+//     super(name);
+//     this._certifications = certifications;
+// } 
+// }
+
+// const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+
+// nurseOlynyk.takeVacationDays(5);
+// console.log(nurseOlynyk.remainingVacationDays);
+
+
+// Codecademy - Learn Javascript, Classes, Exercise 10, Inheritance V
 class HospitalEmployee {
     constructor(name) {
     this._name = name;
@@ -1172,16 +1205,25 @@ class HospitalEmployee {
 }
 
 class Nurse extends HospitalEmployee {
-constructor(name, certifications) {
+    constructor(name, certifications) {
     super(name);
     this._certifications = certifications;
-} 
+    } 
+    
+    get certifications() {
+    return this._certifications;
+    }
+    
+    addCertification(newCertification) {
+    this._certifications.push(newCertification);
+    }
 }
 
 const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
-
 nurseOlynyk.takeVacationDays(5);
 console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
 
 
 
