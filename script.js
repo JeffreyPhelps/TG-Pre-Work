3c96f512299da2614f363498bbb17e8772bac4c7
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 22:48:04 -0600

    Codecademy - Learn Javascript, Requests, Exercise 5, XHR GET Requests IV

--- script.js
+++ script.js
@@ -1706,19 +1706,62 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Requests, Exercise 5, XHR GET Requests III
+// // Information to reach API
+// const url = 'https://api.datamuse.com/words?';
+// const queryParams = 'rel_rhy=';
+
+// // Selecting page elements
+// const inputField = document.querySelector('#input');
+// const submit = document.querySelector('#submit');
+// const responseField = document.querySelector('#responseField');
+
+// // AJAX function
+// const getSuggestions = () => {
+//   const wordQuery = inputField.value;
+//   const endpoint = `${url}${queryParams}${wordQuery}`;
+  
+//   const xhr = new XMLHttpRequest();
+//   xhr.responseType = 'json';
+
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState === XMLHttpRequest.DONE) {
+//       renderResponse(xhr.response);
+//     }
+//   }
+  
+//   xhr.open('GET', endpoint);
+//   xhr.send();
+// }
+
+// // Clear previous results and display results to webpage
+// const displaySuggestions = (event) => {
+//   event.preventDefault();
+//   while(responseField.firstChild){
+//     responseField.removeChild(responseField.firstChild);
+//   }
+//   getSuggestions();
+// }
+
+// submit.addEventListener('click', displaySuggestions);
+
+
+// Codecademy - Learn Javascript, Requests, Exercise 5, XHR GET Requests IV
 // Information to reach API
 const url = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_rhy=';
+const queryParams = 'rel_jjb=';
+const additionalParams = '&topics=';
 
 // Selecting page elements
 const inputField = document.querySelector('#input');
+const topicField = document.querySelector('#topic');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
 // AJAX function
 const getSuggestions = () => {
   const wordQuery = inputField.value;
-  const endpoint = `${url}${queryParams}${wordQuery}`;
+  const topicQuery = topicField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
   
   const xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
