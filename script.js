ff6b09bb3c4c7fd886d59d5c88dba109e9d79ffa
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 23:20:40 -0600

    Codecademy - Learn Javascript, Requests II, Exercise 9, fetch() POST Requests IV

--- script.js
+++ script.js
@@ -1976,6 +1976,43 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Requests II, Exercise 8, fetch() POST Requests III
+// // Information to reach API
+// const apiKey = '<Your API Key>';
+// const url = 'https://api.rebrandly.com/v1/links';
+
+// // Some page elements
+// const inputField = document.querySelector('#input');
+// const shortenButton = document.querySelector('#shorten');
+// const responseField = document.querySelector('#responseField');
+
+// // AJAX functions
+// const shortenUrl = () => {
+//   const urlToShorten = inputField.value;
+//   const data = JSON.stringify({destination: urlToShorten})
+  
+// 	fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json',
+//       'apikey': apiKey
+//     },
+//     body: data
+//   })
+// }
+
+// // Clear page and call AJAX functions
+// const displayShortUrl = (event) => {
+//   event.preventDefault();
+//   while(responseField.firstChild){
+//     responseField.removeChild(responseField.firstChild)
+//   }
+//   shortenUrl();
+// }
+
+// shortenButton.addEventListener('click', displayShortUrl);
+
+
+// Codecademy - Learn Javascript, Requests II, Exercise 9, fetch() POST Requests IV
 // Information to reach API
 const apiKey = '<Your API Key>';
 const url = 'https://api.rebrandly.com/v1/links';
@@ -1997,6 +2034,13 @@ const shortenUrl = () => {
       'apikey': apiKey
     },
     body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
   })
 }
 
