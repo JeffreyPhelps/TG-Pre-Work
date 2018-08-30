9d5827fc066cba3808d76512c950246836d03a3c
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 20:27:36 -0600

    Codecademy - Learn Javascript, Objects, Exercise 8, Methods

--- script.js
+++ script.js
@@ -754,16 +754,49 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Objects, Exercise 7, Editing a Property
+// let person = {
+//     name: 'Tyron',
+//     age: 40,
+//     weekendAlarm: 'No alarms needed',
+//     weekAlarm: 'Alarm set to 7AM'
+// };
+
+// person.hobbies = ['Basketball', 'Coaching'];
+// person.hobbies = ['Basketball'];
+// console.log(person.hobbies);
+// console.log(person['name']);
+// console.log(person['age']);
+
+// let day = 'Tuesday';
+// let alarm;
+
+// if (day === 'Saturday' || day === 'Sunday' ) {
+//     alarm = 'weekendAlarm';
+// } else {
+//     alarm = 'weekAlarm';
+// }
+
+// console.log(person[alarm]);
+
+
+// Codecademy - Learn Javascript, Objects, Exercise 8, Methods
 let person = {
     name: 'Tyron',
     age: 40,
     weekendAlarm: 'No alarms needed',
-    weekAlarm: 'Alarm set to 7AM'
+    weekAlarm: 'Alarm set to 7AM',
+    sayHello: () => {
+    return 'Hello, there!';
+    }
 };
+
+console.log(person.sayHello());
 
 person.hobbies = ['Basketball', 'Coaching'];
 person.hobbies = ['Basketball'];
 console.log(person.hobbies);
+
+
 console.log(person['name']);
 console.log(person['age']);
 
@@ -777,7 +810,6 @@ if (day === 'Saturday' || day === 'Sunday' ) {
 }
 
 console.log(person[alarm]);
-
 
 
 
