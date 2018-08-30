d6015a8b197f67cafd103bcbb9a118572aa315f0
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 22:50:42 -0600

    Codecademy - Learn Javascript, Requests, Exercise 7, XHR POST Requests I

--- script.js
+++ script.js
@@ -1745,48 +1745,51 @@ console.log('But this just being short of jaundice all the time confused them.')
 // submit.addEventListener('click', displaySuggestions);
 
 
-// Codecademy - Learn Javascript, Requests, Exercise 5, XHR GET Requests IV
-// Information to reach API
-const url = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_jjb=';
-const additionalParams = '&topics=';
+// Codecademy - Learn Javascript, Requests, Exercise 6, XHR GET Requests IV
+// // Information to reach API
+// const url = 'https://api.datamuse.com/words?';
+// const queryParams = 'rel_jjb=';
+// const additionalParams = '&topics=';
 
-// Selecting page elements
-const inputField = document.querySelector('#input');
-const topicField = document.querySelector('#topic');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
+// // Selecting page elements
+// const inputField = document.querySelector('#input');
+// const topicField = document.querySelector('#topic');
+// const submit = document.querySelector('#submit');
+// const responseField = document.querySelector('#responseField');
 
-// AJAX function
-const getSuggestions = () => {
-  const wordQuery = inputField.value;
-  const topicQuery = topicField.value;
-  const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
+// // AJAX function
+// const getSuggestions = () => {
+//   const wordQuery = inputField.value;
+//   const topicQuery = topicField.value;
+//   const endpoint = `${url}${queryParams}${wordQuery}${additionalParams}${topicQuery}`;
   
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
+//   const xhr = new XMLHttpRequest();
+//   xhr.responseType = 'json';
 
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
-    }
-  }
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState === XMLHttpRequest.DONE) {
+//       renderResponse(xhr.response);
+//     }
+//   }
   
-  xhr.open('GET', endpoint);
-  xhr.send();
-}
+//   xhr.open('GET', endpoint);
+//   xhr.send();
+// }
 
-// Clear previous results and display results to webpage
-const displaySuggestions = (event) => {
-  event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
-  }
-  getSuggestions();
-}
+// // Clear previous results and display results to webpage
+// const displaySuggestions = (event) => {
+//   event.preventDefault();
+//   while(responseField.firstChild){
+//     responseField.removeChild(responseField.firstChild);
+//   }
+//   getSuggestions();
+// }
 
-submit.addEventListener('click', displaySuggestions);
+// submit.addEventListener('click', displaySuggestions);
 
+
+// Codecademy - Learn Javascript, Requests, Exercise 7, XHR POST Requests I
+// N/A
 
 
 
