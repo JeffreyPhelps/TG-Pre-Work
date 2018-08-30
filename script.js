ea56ec767d79ed54fc1b6430f0ffa9682a7925a1
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 23:14:13 -0600

    Codecademy - Learn Javascript, Requests II, Exercise 5, fetch() GET Requests IV

--- script.js
+++ script.js
@@ -1880,6 +1880,43 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Requests II, Exercise 4, fetch() GET Requests III
+// // Information to reach API
+// const url = 'https://api.datamuse.com/words';
+// const queryParams = '?sl=';
+
+// // Selects page elements
+// const inputField = document.querySelector('#input');
+// const submit = document.querySelector('#submit');
+// const responseField = document.querySelector('#responseField');
+
+// // AJAX function
+// const getSuggestions = () => {
+//   const wordQuery = inputField.value;
+//   const endpoint = `${url}${queryParams}${wordQuery}`;
+  
+//   fetch(endpoint).then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error('Request failed!');
+//   }, networkError => {
+//     console.log(networkError.message)
+//   })
+// }
+
+// // Clears previous results and display results to webpage
+// const displaySuggestions = (event) => {
+//   event.preventDefault();
+//   while(responseField.firstChild){
+//     responseField.removeChild(responseField.firstChild);
+//   }
+//   getSuggestions();
+// };
+
+// submit.addEventListener('click', displaySuggestions);
+
+
+// Codecademy - Learn Javascript, Requests II, Exercise 5, fetch() GET Requests IV
 // Information to reach API
 const url = 'https://api.datamuse.com/words';
 const queryParams = '?sl=';
@@ -1901,6 +1938,8 @@ const getSuggestions = () => {
     throw new Error('Request failed!');
   }, networkError => {
     console.log(networkError.message)
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
   })
 }
 
@@ -1914,7 +1953,6 @@ const displaySuggestions = (event) => {
 };
 
 submit.addEventListener('click', displaySuggestions);
-
 
 
 
