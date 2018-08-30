0c2fd74fb9ea32409148b62a6f0014fa9eba7e92
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 23:33:17 -0600

    Codecademy - Learn Javascript, Requests II, Exercise 16, async POST Requests III

--- script.js
+++ script.js
@@ -2164,20 +2164,66 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Requests II, Exercise 15, async POST Requests II
-const getData = async () => {
-    try {
-      const response = await fetch('https://api-to-call.com/endpoint', {
-        method: 'POST',
-        body: JSON.stringify({id: 200})
-      })
-      if(response.ok){
-        const jsonResponse = await response.json();
-        return jsonResponse;
+// const getData = async () => {
+//     try {
+//       const response = await fetch('https://api-to-call.com/endpoint', {
+//         method: 'POST',
+//         body: JSON.stringify({id: 200})
+//       })
+//       if(response.ok){
+//         const jsonResponse = await response.json();
+//         return jsonResponse;
+//       }
+//       throw new Error('Request failed!');
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   }
+
+
+// Codecademy - Learn Javascript, Requests II, Exercise 16, async POST Requests III
+// information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+// Code goes here
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url, {
+			method: 'POST',
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+				'apikey': apiKey
       }
-      throw new Error('Request failed!');
-    } catch(error) {
-      console.log(error);
+    });
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
     }
+  } catch (error) {
+    console.log(error);
   }
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
 
 
