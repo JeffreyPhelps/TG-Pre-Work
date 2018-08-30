06547ece9a5676718eaa4470fdd661406fac23ea
Jeffrey <phelpsjeffrey@yahoo.com>
2018-08-29 23:35:54 -0600

    Codecademy - Learn Javascript, Requests II, Exercise 17, Review

--- script.js
+++ script.js
@@ -2182,48 +2182,49 @@ console.log('But this just being short of jaundice all the time confused them.')
 
 
 // Codecademy - Learn Javascript, Requests II, Exercise 16, async POST Requests III
-// information to reach API
-const apiKey = '<Your API Key>';
-const url = 'https://api.rebrandly.com/v1/links';
+// // information to reach API
+// const apiKey = '<Your API Key>';
+// const url = 'https://api.rebrandly.com/v1/links';
 
-// Some page elements
-const inputField = document.querySelector('#input');
-const shortenButton = document.querySelector('#shorten');
-const responseField = document.querySelector('#responseField');
+// // Some page elements
+// const inputField = document.querySelector('#input');
+// const shortenButton = document.querySelector('#shorten');
+// const responseField = document.querySelector('#responseField');
 
-// AJAX functions
-// Code goes here
-const shortenUrl = async () => {
-	const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
-  try {
-    const response = await fetch(url, {
-			method: 'POST',
-      body: data,
-      headers: {
-        'Content-type': 'application/json',
-				'apikey': apiKey
-      }
-    });
-		if(response.ok){
-      const jsonResponse = await response.json();
-      renderResponse(jsonResponse);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+// // AJAX functions
+// // Code goes here
+// const shortenUrl = async () => {
+// 	const urlToShorten = inputField.value;
+//   const data = JSON.stringify({destination: urlToShorten});
+//   try {
+//     const response = await fetch(url, {
+// 			method: 'POST',
+//       body: data,
+//       headers: {
+//         'Content-type': 'application/json',
+// 				'apikey': apiKey
+//       }
+//     });
+// 		if(response.ok){
+//       const jsonResponse = await response.json();
+//       renderResponse(jsonResponse);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-// Clear page and call AJAX functions
-const displayShortUrl = (event) => {
-  event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
-  }
-  shortenUrl();
-}
+// // Clear page and call AJAX functions
+// const displayShortUrl = (event) => {
+//   event.preventDefault();
+//   while(responseField.firstChild){
+//     responseField.removeChild(responseField.firstChild);
+//   }
+//   shortenUrl();
+// }
 
-shortenButton.addEventListener('click', displayShortUrl);
+// shortenButton.addEventListener('click', displayShortUrl);
 
 
-
+// Codecademy - Learn Javascript, Requests II, Exercise 17, Review
+// N/A
